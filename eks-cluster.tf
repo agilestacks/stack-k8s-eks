eafabb4b0f585b0a8bdfeacc927b6d5799296fbc
Arkadi Shishlov <arkadi@agilestacks.com>
2019-10-15 22:28:21 +0300

    Kubernetes 1.14

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -89,7 +89,7 @@ resource "aws_eks_cluster" "main" {
 }
 
 locals {
-  version = "1.13"
+  version = "1.14"
   api_endpoint = "${replace(aws_eks_cluster.main.endpoint, "/https://([^/]+).*/", "$1")}"
   api_endpoint_host = "${replace(local.api_endpoint, "/([^:]+).*/", "$1")}"
 }
