2919caf83648353a45ab22324818ae1155f159a1
Arkadi Shishlov <arkadi@agilestacks.com>
2021-08-05 13:52:21 +0300

    Better DNS zone import

--- bin/route53-zone-by-domain.sh
+++ bin/route53-zone-by-domain.sh
@@ -7,6 +7,8 @@
 AWS="${AWS:-aws}"
 JQ="${JQ:-jq}"
 DOMAIN="$1"
+PRIVATE_ZONE="${2:-false}"
+
 if test -z "$DOMAIN"; then echo "Usage: $0 <domain.name>"; exit 1; fi
 
 if test -n "$EXTERNAL_AWS_ACCESS_KEY" -a -n "$EXTERNAL_AWS_SECRET_KEY"; then
@@ -16,11 +18,12 @@ if test -n "$EXTERNAL_AWS_ACCESS_KEY" -a -n "$EXTERNAL_AWS_SECRET_KEY"; then
   export AWS_SECRET_ACCESS_KEY=$EXTERNAL_AWS_SECRET_KEY
 fi
 
-route53_zones_resp=$($AWS --output=json route53 list-hosted-zones-by-name --dns-name "$DOMAIN" --max-items 1)
-zone=$($JQ .HostedZones[0] <<< $route53_zones_resp)
+route53_zones_resp=$($AWS --output=json route53 list-hosted-zones-by-name --dns-name "$DOMAIN" --max-items=2)
+# TODO what happens if there are two+ (private) zones?
+zone=$($JQ ".HostedZones[] | select(.Config.PrivateZone == $PRIVATE_ZONE)" <<< $route53_zones_resp)
 test -z "$zone" -o "$zone" = "null" && exit 0
 
-name=$($JQ -r .Name <<< $zone)
+name=$($JQ -r "select(.Name == \"${DOMAIN}.\") | .Name" <<< $zone)
 test "$DOMAIN" != "$name" -a "${DOMAIN}." != "$name" && exit 0
 
 next_dns_name=$($JQ -r .NextDNSName <<< $route53_zones_resp)
@@ -29,5 +32,5 @@ if test "$name" = "$next_dns_name"; then
     exit 1
 fi
 
-id=$($JQ -r .Id <<< $zone | sed -e 's|/hostedzone/||')
+id=$($JQ -r "select(.Name == \"${DOMAIN}.\") | .Id" <<< $zone | sed -e 's|/hostedzone/||')
 echo $id
